a795fd4b707d26292e76977b9d2e8918b06df587
LeinneSW <nno88551@naver.com>
2025-09-19 14:31:35 +0900

    기록 페이지 내 곡 레벨 출력 로직 추가

--- webui/js/music.js
+++ webui/js/music.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     fetch('static/resources/music_info.json').then(async r => {
         const musicData = await r.json();
-        musicData && document.querySelectorAll('tr.paginated-content').forEach(tr => {
-            const id = tr.getAttribute('data-music-id');
-            const name = musicData[id]?.name || `알 수 없음(${id})`;
-            const el = tr.querySelector('.song-name');
-            if(el) el.textContent = name;
+        if(!musicData) return;
+        document.querySelectorAll('tr').forEach(tr => {
+            const nameElement = tr.querySelector('td.song-name');
+            if(!nameElement) return;
+
+            const musicInfo = musicData[tr.dataset.musicId] || {};
+            if(!musicInfo.name){
+                nameElement.textContent = `미등록 곡(${id})`
+                return;
+            }
+            let name = musicInfo.name;
+            if(tr.dataset.seq != null && musicInfo.level[tr.dataset.seq] != null){
+                name += `(${musicInfo.level[tr.dataset.seq]})`;
+            }
+            nameElement.textContent = name;
         });
     })
 });
